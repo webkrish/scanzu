822069ec3216eb2bbb643eb8ba5a282e4f88f394
Krishnendu Tripathy <krishnendutripathy337@gmail.com>
2023-09-26 13:24:51 +0530

    a

--- js/script.js
+++ js/script.js
@@ -1,32 +1,34 @@
 let navBar = document.getElementById("my-nav");
-        window.onscroll = function () {
-            if (window.scrollY > 22) {
-                navBar.classList.add("scrolled");
-            } else {
-                navBar.classList.remove("scrolled");
-            }
-        };
+window.onscroll = function () {
+    if (window.scrollY > 22) {
+        navBar.classList.add("scrolled");
+    } else {
+        navBar.classList.remove("scrolled");
+    }
+};
 
-        
-        var swiper = new Swiper(".mySwiper", {
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    autoplay: true,
+    loop: true,
+    grabCursor: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        580: {
+            slidesPerView: 2,
+        },
+        990: {
             slidesPerView: 3,
-            spaceBetween: 30,
-            autoplay: true,
-            loop: true,
-            grabCursor:true,
-            pagination: {
-                el: ".swiper-pagination",
-                clickable: true
-            },
-            breakpoints:{
-                0:{
-                    slidesPerView: 1,
-                },
-                580:{
-                    slidesPerView: 2,
-                },
-                990:{
-                    slidesPerView: 3,
-                },
-            }
-        });
+        },
+    }
+});
+
+
